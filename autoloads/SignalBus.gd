342b488d85ef6768a67a367b6645a3be391482b1
Jack-Kaller <157049895+Jack-Kaller@users.noreply.github.com>
2025-07-31 21:11:22 +0200

    feat(ANOTH-6): add simple track race (#2)
    * feat(ANOTH-6): add simple track race
    
    * add start race
    
    * Corrects the collider to avoid unwanted drifting
    
    * add startArea
    
    ---------
    
    Co-authored-by: Gromnax <hezon.morgan@orange.fr>

---
+++ autoloads/SignalBus.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+@warning_ignore("unused_signal")
+signal player_entered_start_area(player: Car)
--- scenes/carcontroller/car_controller.gd
+++ scenes/carcontroller/car_controller.gd
@@ -6,12 +6,3 @@ class_name CarController
 var power : float ##Gas minus brake
 var steering : float ##Steering, left minus right
 var jump : bool = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
