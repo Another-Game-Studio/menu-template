21fe7c9f35b2a07d6ddfd52da69e8eb3f5dec8fd
Gromnax <hezon.morgan@orange.fr>
2025-08-01 20:37:59 +0200

    Add explosive barrel
    The explosive barrel does not yet kill the player

---
+++ scenes/obstacles/explosive_barrel.gd
@@ -0,0 +1,23 @@
+extends RigidBody3D
+
+const Explosion : PackedScene = preload("res://addons/PolyBlocks/EffectBlocks/assets/explosions/explosion_small.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func explode() -> void:
+	var explosion : Node3D = Explosion.instantiate()
+	add_child(explosion)
+	explosion.explosion()
+	queue_free()
+
+func _on_body_entered(body: PhysicsBody3D) -> void:
+	print("Body entered:"+str(body))
+	if body.get_collision_layer_value(1):
+		explode()
